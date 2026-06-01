186344c0df74d7e37fb5f1e5e7ecebbec10e93e6
Rohan Anil Mohite <rohanmohite2001@gmail.com>
2026-06-01 18:57:40 +0530

    Initial Flask project

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Welcome to My First Flask Website</h1>
+    <p>Deployed from GitHub!</p>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
