b3375e27d50a44923d59996aecf4b9630e396856
Rohan Anil Mohite <rohanmohite2001@gmail.com>
2026-06-01 19:31:31 +0530

    Added home about contact pages

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>Welcome to My First Flask Website</h1>
-    <p>Deployed from GitHub!</p>
-    """
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
